c2f52df9001960e26ab31842791a5f1011e252a3
sajedeh gharabadiyan <sajdehgharabadiyan78@gmail.com>
2020-12-23 14:56:40 -0800

    Homework8

---
+++ OS8/1.sh
@@ -0,0 +1,4 @@
+git config --global user.name "sajedeh gharabadiyan"
+git config --global user.email "gharabadiyansajedeh@gmail.com"
+git config --global user.password "9876"
+git config --global credential.helper store
